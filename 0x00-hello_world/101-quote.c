2ab4643d30dccb8c483a0977cfc602d8e311cbec
djarrel <jeffdarrellthegreat@gmail.com>
2022-06-29 05:43:47 -0700

    using write to print out instead of puts and printf

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+/**
+ *main - prints and that piece of art is useful\" - Dora Korpar, 2015-10-19.
+ *Return: 1 is success.
+*/
+
+int main(void)
+{
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19", 58);
+	return (1);
+}
