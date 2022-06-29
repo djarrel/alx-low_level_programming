70be28efdce439a4bbc9a4d25d974594e63004df
djarrel <jeffdarrellthegreat@gmail.com>
2022-06-29 04:02:48 -0700

    learning c programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 
+/**
+ *main-it serves a starting point for code execution.
+ *Return: 0 Zero means success, non zero means error.
+*/
 int main(void)
 {
 
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 
 }
