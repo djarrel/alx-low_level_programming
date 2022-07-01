d0cc646263d88610dc0a144af7c8fe6a708fe38c
djarrel <jeffdarrellthegreat@gmail.com>
2022-07-01 06:03:47 -0700

    print single digits with commas and space

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers with commas and spaces
+ * Return: 0 shows success
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+		if (i == '9')
+			continue;
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
