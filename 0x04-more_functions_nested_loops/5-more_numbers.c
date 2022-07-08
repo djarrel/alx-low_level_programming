b0147af1e15ec215fa16faf5705d8926293b4682
djarrel <jeffdarrellthegreat@gmail.com>
2022-07-07 18:42:55 -0700

    nested loop

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,11 +9,11 @@ void more_numbers(void)
 	int n;
 	int i;
 
-	for (n = 0; n <= 10; n++)
+	for (n = 0; n <= 9; n++)
 	{
 		for (i = 0; i <= 14; i++)
 		{
-			if ((i / 10) != 0)
+			if (i > 9)
 			{
 				_putchar(i / 10 + '0');
 			}
