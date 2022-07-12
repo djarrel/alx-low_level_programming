fb8090681557832d78beb10f90c67b6bb4f142d9
djarrel <jeffdarrellthegreat@gmail.com>
2022-07-12 07:03:03 -0700

    copy string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * *_strcpy - copies the string pointed to by src includeing '\0'
+ * @dest: char type string
+ * @src: char type string
+ * Return: pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = -1; src[i] != 0; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
+
