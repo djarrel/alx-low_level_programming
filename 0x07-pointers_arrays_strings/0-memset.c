9d37a64893beaa70bea46776e86d415625730901
djarrel <jeffdarrellthegreat@gmail.com>
2022-07-18 04:19:01 -0700

    fill memory with constant byte

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -3,14 +3,15 @@
  * _memset - set memory
  * @s: pointer to a string
  * @b: charcter to be changed
- * @n: space to be used
+ * @n: space to be useid
  * Return: return s
  */
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
-	for (i = 0; i < n; i++)
+	i = 0;
+	while (i < n)
 	{
 		s[i] = b;
 		i++
