37009e1b80f3488ab153ccc3570d777c63406761
djarrel <jeffdarrellthegreat@gmail.com>
2022-07-20 02:58:17 -0700

    power

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _pow_recursion - power of integer using recursion
+ * @x: integer to be powered
+ * @y: power
+ * Return: x the value of power
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y == 0)
+	{
+		return (1);
+	}
+	if (y > 0)
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+	return (x);
+}
