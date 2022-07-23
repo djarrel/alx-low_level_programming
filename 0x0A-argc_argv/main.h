bf15a2ba44b3663533245b8e0c74187edd2eb649
djarrel <jeffdarrellthegreat@gmail.com>
2022-07-23 06:30:16 -0700

    prints all arguments

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -3,5 +3,6 @@
 #include <stdio.h>
 int main(int __attribute__((__unused__))argc, char *argv[]);
 int main(int argc, char __attribute__((__unused__)) *argv[]);
+int main(int argc, char *argv[]);
 
 #endif
