1defac54ed200702619ed60bcdb59f1d60d22a69
djarrel <jeffdarrellthegreat@gmail.com>
2022-07-23 05:54:16 -0700

    prints program name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - print program name
+ * @argc: integer count value of argv
+ * @argv: pointer to a string
+ * Return: 0 success
+ */
+
+char main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
