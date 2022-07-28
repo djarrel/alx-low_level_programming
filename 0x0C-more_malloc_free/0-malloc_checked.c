ed509f4b52ce8ed27c7f172aa5dbf741d61fbc04
djarrel <jeffdarrellthegreat@gmail.com>
2022-07-28 08:58:23 -0700

    allocated space

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b:unsigned int of amount of needed memory
+ * Return: void
+ */
+
+void *malloc_checked(unsigned int b)
+{
+
+	void *s;
+	
+	s = malloc(b);
+	if (s == NULL)
+	{
+		exit(98);
+	}
+	return(s);
+}
