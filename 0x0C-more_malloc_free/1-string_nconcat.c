5dfc38c7cddc49ad54c036bba1fb8efec64b61be
djarrel <jeffdarrellthegreat@gmail.com>
2022-07-28 18:43:03 -0700

    concatenates

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,38 +10,32 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *c;
-	unsigned int i, j = n;
+	char *concat;
+	unsigned int len = n, index;
 
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
+
 	if (s2 == NULL)
-	{
 		s2 = "";
-	}
 
-	for (i = 0; s1[i]; i++)
-	{
-		j++;
-	}
+	for (index = 0; s1[index]; index++)
+		len++;
 
-	c = malloc(sizeof(char) * (j + 1));
-	if (c == NULL)
-	{
+	concat = malloc(sizeof(char) * (len + 1));
+
+	if (concat == NULL)
 		return (NULL);
-	}
-	j = 0;
 
-	for (i = 0; s1[i]; i++)
-	{
-		c[j++] = s1[i];
-	}
-	for (i = 0; s2[i]; i++)
-	{
-		c[j++] = s2[i];
-	}
-	c[j] = '\0';
-	return (c);
+	len = 0;
+
+	for (index = 0; s1[index]; index++)
+		concat[len++] = s1[index];
+
+	for (index = 0; s2[index] && index < n; index++)
+		concat[len++] = s2[index];
+
+	concat[len] = '\0';
+
+	return (concat);
 }
