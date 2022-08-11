2d4c6b1a7d1ff90038b54291f73c45a8e39d027f
djarrel <jeffdarrellthegreat@gmail.com>
2022-08-11 13:57:51 -0700

    print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,9 +13,9 @@ size_t print_list(const list_t *h)
 	for (; temp; temp = temp->next, i++)
 	{
 		if (temp->str == NULL)
-        	{
-                	printf("[0] (nil)\n");
-        	}
+		{
+			printf("[0] (nil)\n");
+		}
 		else
 		{
 			printf("[%d] %s\n", temp->len, temp->str);
