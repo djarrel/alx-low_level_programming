e7c44d886dc6415c68c323e2789901917643b02b
djarrel <jeffdarrellthegreat@gmail.com>
2022-08-11 05:49:25 -0700

    print list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+/**
+ * print_list - prints list
+ * @h: pointer to a struct of typedef list_l
+ * Return: number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	unsigned int i = 0;
+
+	while (h)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		i++;
+		h = h->next;
+	}
+	return (i);
+}
