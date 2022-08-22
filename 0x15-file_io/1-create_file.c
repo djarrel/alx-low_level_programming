91813e6c104f7da9109a193b4660b1c64ce37373
djarrel <jeffdarrellthegreat@gmail.com>
2022-08-22 10:13:38 -0700

    fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,9 +15,12 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 	count = 0;
-	for (i = 0; text_content[i]; i++)
+	if (text_content != NULL)
 	{
-		count++;
+		for (i = 0; text_content[i]; i++)
+		{
+			count++;
+		}
 	}
 
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
